c75c39f5f1d08e78adca39fd65131bef63432736
Amr20533 <amrelnemr638@gmail.com>
2025-01-06 12:48:31 +0200

    Edit/Delete Task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('tasks/', getTasks ,name= "get-all-tasks"),
+    path('tasks/task/<int:task_id>', getTaskById ,name= "get-task"),
     path('tasks/addTask', addNewTask ,name= "add-task"),
+    path('tasks/editTask/<int:task_id>', editTask ,name= "edit-task"),
+    path('tasks/deletTask/<int:task_id>', deleteTask ,name= "delete-task"),
 
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -18,6 +18,20 @@ def getTasks(request):
     return Response({"tasks" : task_serializer.data}, status= status.HTTP_200_OK)
 
 
+@api_view(['GET'])
+def getTaskById(request, task_id):
+    try:
+        task = get_object_or_404(Task, id=task_id)
+
+        task_serializer = TaskSerializer(task, many = False)
+
+        return Response({"tasks" : task_serializer.data}, status= status.HTTP_200_OK)
+    
+    except Task.DoesNotExist as e:
+        return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def addNewTask(request):
@@ -44,6 +58,54 @@ def addNewTask(request):
         serializer = TaskSerializer(task)
 
         return Response({"task": serializer.data}, status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+def editTask(request, task_id):
+    data = request.data
+    try:
+        task = get_object_or_404(Task, id=task_id)
+
+        due_date = data.get('due_date')
+        if due_date:
+            if isinstance(due_date, str):
+                naive_due_date = timezone.datetime.fromisoformat(due_date)
+            else:
+                naive_due_date = due_date
+            aware_due_date = timezone.make_aware(naive_due_date)
+        else:
+            aware_due_date = task.due_date 
+
+        task.title = data.get('title', task.title)
+        task.description = data.get('description', task.description)
+        task.priority = data.get('priority', task.priority)
+        task.due_date = aware_due_date
+        task.status = data.get('status', task.status)
+        task.image = data.get('image', task.image)
+
+        task.save()
+
+        serializer = TaskSerializer(task)
+
+        return Response({"task": serializer.data}, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteTask(request, task_id):
+    try:
+        task = get_object_or_404(Task, id=task_id)
+
+        task.delete()
+
+        return Response({"detail" : "Task is Successfully Deleted!!"}, status= status.HTTP_200_OK)
+    
+    except Task.DoesNotExist as e:
+        return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
 
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
